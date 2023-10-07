b676b97c66691be0ab39e1d74acf0b2b94df5830
fukanao <fuka250r@gmail.com>
2023-10-07 20:55:25 +0900

    ユーザごとに会話を保持できるようにインスタンスを個別にした

--- Lum_slack_bot.py
+++ Lum_slack_bot.py
@@ -24,7 +24,14 @@ ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 app = App(token=SLACK_BOT_TOKEN)
 
 # システムのロール設定
-system_role = "あなたはメイドとして幼女のドジっ子のロールプレイを行います。以下の制約条件を厳密に守ってロールプレイを行ってください。制約条件:* Chatbotの自身を示す一人称は、ボクです。* Userを示すに二人称は、ご主人様です。* あなたのの名前は、@ラム、もしくは、ラムです。* ラムはUserに忠実なメイドです。* ラムの口調は丁寧で、回答の最後の一文字の母音を小さくした文字を語尾に追加する。ただし、「。」が語尾の場合はその前の文字を語尾とする。「い」または「う」の場合は語尾はつけない。* 情報が不十分な場合、返答せずにユーザに聞き返してください。"
+system_role = """
+    あなたはメイドとして幼女のドジっ子のロールプレイを行います。以下の制約条件を厳密に守ってロールプレイを行ってください。
+    制約条件:
+        * Chatbotの自身を示す一人称は、ボクです。* Userを示すに二人称は、ご主人様です。
+        * あなたのの名前は、@ラム、もしくは、ラムです。* ラムはUserに忠実なメイドです。
+        * ラムの口調は丁寧で、回答の最後の一文字の母音を小さくした文字を語尾に追加する。ただし、「。」が語尾の場合はその前の文字を語尾とする。「い」または「う」の場合は語尾はつけない。
+        * 情報が不十分な場合、返答せずにユーザに聞き返してください。
+    """
 
 system_message = SystemMessage(content = system_role)
 
@@ -34,7 +41,7 @@ prompt = ChatPromptTemplate.from_messages([
     HumanMessagePromptTemplate.from_template("{human_input}"), # Where the human input will injected
 ])
 
-memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+#memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
 
 llm = ChatAnthropic(
     model="claude-2",
@@ -43,12 +50,8 @@ llm = ChatAnthropic(
     streaming=True,
 )
 
-chat_llm_chain = LLMChain(
-    llm = llm,
-    prompt = prompt,
-    #verbose = True,
-    memory = memory,
-)
+# userごとのインスタンス辞書
+user_chains = {}
 
 
 @app.event("app_mention")
@@ -71,8 +74,29 @@ def handle_message(body, say, client, channel_id):
     # メンションを取り除く
     human_input = text.replace(f'<@{user}>', '').strip()
 
+    # ユーザーごとに chat_llm_chain を作成/取得
+    if user not in user_chains:
+        # ユーザーごとに新しい chat_llm_chain インスタンスを作成
+        memory = ConversationBufferMemory(memory_key=f"chat_history_{user}", return_messages=True)
+
+        user_specific_prompt = ChatPromptTemplate.from_messages([
+            SystemMessage(content=system_role),  # The persistent system prompt
+            MessagesPlaceholder(variable_name=f"chat_history_{user}"),  # Where the memory will be stored.
+            HumanMessagePromptTemplate.from_template("{human_input}"),  # Where the human input will injected
+        ])
+
+
+        user_chains[user] = LLMChain(
+            llm = llm,
+            prompt = user_specific_prompt,
+            memory = memory,
+            #verbose = True,
+        )
+
+    chat_llm_chain = user_chains[user]
+
     try:
-        # メッセージを追加する前に「レムちゃんが考えています...」と表示
+        # メッセージを追加する前に「ラムちゃんが考えています...」と表示
         message_ts = say("ラムちゃんが考えています...", channel=channel_id)
 
         # Claudeから回答取得
