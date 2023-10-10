27aa33b1c2efaa3cc659a408e2b7909cbd2e642b
fukanao <fuka250r@gmail.com>
2023-10-10 10:35:19 +0900

    エラーは出なくなったけど、効果がイマイチ

--- Lum_slack_bot.py
+++ Lum_slack_bot.py
@@ -99,49 +99,38 @@ def handle_message(body, say, client, channel_id):
         # メッセージを追加する前に「ラムちゃんが考えています...」と表示
         message_ts = say("ラムちゃんが考えています...", channel=channel_id)
 
-
-        def chat_claude(human_input):
-            resp = chat_llm_chain.predict(human_input=human_input)
-            response_text = ""
-            for chunk in resp:
-                if chunk:
-                    content = chunk
-                    if content:
-                        response_text += content
-                        yield content
-
-        
-
         # Stream
         response_text = ""
         last_update = -1
         text = last_post_text = ""
 
-        for sentense in chat_claude(human_input):
+        for sentense in chat_llm_chain.predict(human_input=human_input):
             response_text += sentense
-            if (time.time() - last_update) > .5:
-                last_update = time.time()
-                last_post_text = response_text
+            if response_text != " ": #からの場合除外
+                if (time.time() - last_update) > .4:
+                    last_update = time.time()
+                    last_post_text = response_text
+                    client.chat_update(
+                        channel = channel_id,
+                        ts = message_ts['ts'],
+                        text = response_text
+                    )
+        """
+        print("#120 response_text =",response_text)
+        if response_text != " ": #からの場合除外
+            if last_post_text != response_text:
                 client.chat_update(
                     channel = channel_id,
                     ts = message_ts['ts'],
                     text = response_text
                 )
-
-        if last_post_text != response_text:
-            client.chat_update(
-                channel = channel_id,
-                ts = message_ts['ts'],
-                text = response_text
-            )
-
+        """
 
 
         # Claudeから回答取得
         #response = chat_llm_chain.predict(human_input=human_input)
 
-        #say(response, delete_original="ラムちゃんが考えています...", channel=channel_id)
-        say(text, delete_original="ラムちゃんが考えています...", channel=channel_id)
+        say(response_text, delete_original="ラムちゃんが考えています...", channel=channel_id)
         client.chat_delete(ts=message_ts['ts'], channel=channel_id)
 
     except Exception as e:
