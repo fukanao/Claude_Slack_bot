869d5577953f8a295cf5f3454462f98400476ad9
fukanao <fuka250r@gmail.com>
2023-10-16 09:36:40 +0900

    ローカルベクターストア参照可能に

--- Lum_slack_bot.py
+++ Lum_slack_bot.py
@@ -1,4 +1,6 @@
 import os
+import re
+import openai
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -16,10 +18,19 @@ from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 from langchain.memory import ConversationBufferMemory
 
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.chains import ConversationalRetrievalChain
+from langchain.chains import RetrievalQA
+from langchain.vectorstores import Chroma
+
+
 # トークンを読み込む
 SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
 SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+PERSIST_DIR = os.getenv("PERSIST_DIR")
 
 app = App(token=SLACK_BOT_TOKEN)
 
@@ -46,12 +57,25 @@ prompt = ChatPromptTemplate.from_messages([
 llm = ChatAnthropic(
     model="claude-2",
     temperature=0,
-    max_tokens=10000,
+    max_tokens=50000,
     #streaming=True,
 )
 
 # userごとのインスタンス辞書
 user_chains = {}
+
+
+# claude_ldata用パラメータ
+persist_directory = PERSIST_DIR
+
+embedding = OpenAIEmbeddings()
+#k = 10 #PPPoE OK
+#k = 20 #PPPoE OK
+k = 30 #PPPoE OK
+#k = 40 #PPPoE NG
+#k = 50 #PPPoE NG
+#k = 80 #PPPoE NG
+
 
 
 @app.event("app_mention")
@@ -108,6 +132,60 @@ def handle_message(body, say, client, channel_id):
     except Exception as e:
         say(str(e))
         say('Claude エラーが発生しました。')
+
+
+@app.command("/claude_ldata")
+def command_handler(ack, say, command, client, channel_id):
+    # Always acknowledge the command request first
+    ack()
+    global chat_llm_chain
+
+    #text = body['event']['text']
+    #user = body['event']['user']
+
+
+    try:
+        # メッセージ取得
+        message = re.sub(r'^<.*>', '', command['text'])
+        # スラッシュコマンド内容表示
+        slash_text = command['command'] + ' ' + message
+        say(slash_text, channel=channel_id)
+
+        response = client.chat_postMessage(channel=channel_id, text="claude_ldataラムちゃんが考えています...")
+
+        vectordbkwargs = {"search_distance": 0.4} # ベクトル間の距離の閾値
+
+        vectorstore = Chroma(persist_directory=persist_directory, embedding_function=embedding)
+
+        # RetrievalQA
+        db_qa = RetrievalQA.from_chain_type(
+            llm=llm,
+            chain_type="stuff",
+            retriever=vectorstore.as_retriever(search_kwargs={'k':k}),
+            return_source_documents=True,
+            #chain_type_kwargs=chain_type_kwargs
+        )
+
+        result = db_qa({"query": message})
+
+        # show source documents
+        sources = []
+        for doc in result['source_documents']:
+            if 'source' in doc.metadata:
+                source_str = doc.metadata['source']
+                sources.append(source_str)
+
+        #output = result["result"] + "\n\n" + "\n".join(sources)
+        output = result["result"] + "\n\n" + "(k="+str(k)+")"
+
+
+        # メッセージを更新
+        client.chat_update(channel=channel_id, ts=response['ts'], text=output)
+
+    except Exception as e:
+        say(str(e))
+        say('サーバエラーが発生しました。')
+
 
 
 if __name__ == "__main__":
